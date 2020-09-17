f4bc302b57dca9b9724bc2dd9c289e552546e02b
Dionne Stratton <mylife33@hotmail.com>
2020-09-16 19:11:52 -0500

    almost?

---
+++ src/App.js
@@ -0,0 +1,140 @@
+import './App.css';
+import { v4 as uuid } from 'uuid'
+import Form from './Form'
+import React, { useState, useEffect } from 'react'
+import User from './User'
+import schema from './formSchema'
+import axios from 'axios'
+import * as yup from 'yup'
+// 
+// const initialUserList = [
+//   {
+//     id: uuid(),
+//     name: 'Dionne',
+//     email: 'nunyabidness@gmail.com',
+//     password: '1214',
+//     terms: true,
+//   },
+
+// ]
+
+const initialFormValues = {
+  name: '',
+  email: '',
+  password: '',
+  terms: false,
+}
+
+const initialFormErrors = {
+  name: '',
+  email: '',
+  password: '',
+  terms: '',
+}
+const initialUserList = []
+const initialDisabled = true
+
+export default function App() {
+  const [user, setUser] = useState(initialUserList)
+  const [formValues, setFormValues] = useState(initialFormValues)
+  const [formErrors, setFormErrors] = useState(initialFormErrors)
+  const [disabled, setDisabled] = useState(initialDisabled)
+
+  const getUser = () => {
+
+    axios.get('https://reqres.in/api/users')
+      .then(res => {
+        setUser(res.data.data)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
+  const postNewUser = newUser => {
+    axios.post("https://reqres.in/api/users", newUser)
+      .then(res => {
+        setUser(res.data)
+        setFormValues(initialFormValues)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+      .finally(() => {
+      })
+  }
+
+ const validate = (name, value) => {
+   yup
+     .reach(schema, name)
+     // we can then run validate using the value
+     .validate(value)
+     // if the validation is successful, we can clear the error message
+     .then(valid => {
+       setFormErrors({
+         ...formErrors,
+         [name]: ""
+       })
+     })
+     /* if the validation is unsuccessful, we can set the error message to the message 
+       returned from yup (that we created in our schema) */
+     .catch(err => {
+       setFormErrors({
+         ...formErrors,
+         [name]: err.errors[0]
+       });
+     });
+ }
+
+  const inputChange = (name, value) => {
+    // 🔥 STEP 10- RUN VALIDATION WITH YUP
+    validate(name, value)
+    setFormValues({
+      ...formValues,
+      [name]: value // NOT AN ARRAY
+    })
+  }
+
+  const formSubmit = () => {
+    const newUser = {
+      name: formValues.name.trim(),
+      email: formValues.email.trim(),
+      password: formValues.password.trim(),
+      terms: formValues.terms,
+    }
+    postNewUser(newUser)
+  }
+
+    useEffect(() => {
+      getUser()
+    }, [])
+
+  useEffect(() => {
+    schema.isValid(formValues)
+      .then(valid => {
+        setDisabled(!valid)
+      })
+  }, [formValues])
+
+  return (
+    <div className='container'>
+      <header><h1>User Onboarding</h1></header>
+      <Form
+        values={formValues}
+        change={inputChange}
+        submit={formSubmit}
+        disabled={disabled}
+        errors={formErrors}
+        formValues={formValues}
+        postNewUser={postNewUser}
+      />
+      {user.map((newUser, item) => {
+        return (
+          <User key={item} details={newUser} />
+        )
+      })
+      }
+    {/* {console.log(user)} */}
+    </div>
+  )
+}
---
+++ src/Form.js
@@ -0,0 +1,97 @@
+import React from 'react'
+
+export default function Form(props) {
+
+      const {
+        values,
+        submit,
+        change,
+        disabled,
+        errors,
+        formValues,
+        postNewUser,
+      } = props
+
+  const onChange = evt => {
+    const { name, value, type, checked } = evt.target
+    const valueToUse = type === 'checkbox' ? checked : value
+    change(name, valueToUse)
+  }
+
+    const onSubmit = evt => {
+        evt.preventDefault()
+        postNewUser(formValues)
+        submit()
+    }
+
+  return (
+    <form className='form container' onSubmit={onSubmit}>
+      <div className='form-group submit'>
+        <h2>Add a User</h2>
+
+        {/* 🔥 DISABLE THE BUTTON */}
+        <button disabled={disabled}>submit</button>
+
+        <div className='errors'>
+          {/* 🔥 RENDER THE VALIDATION ERRORS HERE */}
+          <div>{errors.name}</div>
+          <div>{errors.email}</div>
+          <div>{errors.password}</div>
+          <div>{errors.terms}</div>
+        </div>
+      </div>
+
+      <div className='form-group inputs'>
+        <h4>General information</h4>
+
+
+        <label>Name
+          <input
+            value={values.name}
+            onChange={onChange}
+            name='name'
+            type='text'
+            placeholder='type your name'
+            maxLength='30'
+          />
+        {/* {errors.name.length > 0 ? <p>{errors.name}</p> : null}   */}
+     </label>
+
+        <label>Email
+ 
+          <input
+            type='email'
+            name='email'
+            onChange={onChange}
+            value={values.email}
+            placeholder='enter your e-mail'
+            maxLength='30'
+          />
+          {errors.email.length > 0 ? <p>{errors.email}</p> : null}
+        </label>
+
+        <label>Password
+
+           <input
+            type='password'
+            name='password'
+            onChange={onChange}
+            value={values.password}
+            placeholder='type a password'
+            maxLength='30'
+          />
+          {errors.password.length > 0 ? <p>{errors.password}</p> : null}
+        </label>
+        <label>Terms
+           <input
+            type='checkbox'
+            name='terms'
+            onChange={onChange}
+            value={values.terms}
+          />
+        </label>
+      </div>
+    </form>
+  )
+}
+
