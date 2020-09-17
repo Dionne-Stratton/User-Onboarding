808e5e667cf740665773fe20c13a96b6f9abe8ad
Dionne Stratton <mylife33@hotmail.com>
2020-09-17 17:21:01 -0500

    a few changes to cypress

--- cypress/integration/form_test.spec.js
+++ cypress/integration/form_test.spec.js
@@ -1,10 +1,8 @@
-
-
 describe("test", () => {
     beforeEach(() => {
         cy.visit("http://localhost:3000")
     })
-    it("Testing Yo", () => {
+    it("Testing for my sanity", () => {
         expect(1 + 2).equal(3)
         expect(1 + 1).equal(2)
         expect(2 + 2).not.to.equal(5)
@@ -15,16 +13,17 @@ describe("test", () => {
     const termsInput = () => cy.get('[type="checkbox"]')
     const submitButton = () => cy.get("button")
 
+    it('check for validation', () => {
+        passwordInput()
+            .type('no')
+        cy.contains('password must be 3 chars or longer').should('exist')
+    })
+
     it("submit test", () => {
-        textInput().type("Testing 123").should("have.value", "Testing 123")
-        emailInput().type("test@test.com").should("have.value", "test@test.com")
-        passwordInput().type("1234").should("have.value", "1234")
+        textInput().type("Nunya Bidnezz").should("have.value", "Nunya Bidnezz")
+        emailInput().type("perry@platapus.com").should("have.value", "perry@platapus.com")
+        passwordInput().type("secret").should("have.value", "secret")
         termsInput().check()
         submitButton().click()
-    })
-    it('should check for validation', () => {
-        passwordInput()
-        .type('ab')
-        cy.contains('password must be 3 chars or longer').should('exist')
     })
 })
